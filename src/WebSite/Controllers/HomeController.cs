df9cb219a33bd7675679c7198a8bfe64f3718c24
Mike Chaliy <mike@chaliy.name>
2011-02-17 14:07:23 +0200

    Added ASP.NET MVC 3.0 version of the site

---
+++ src/WebSite/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
